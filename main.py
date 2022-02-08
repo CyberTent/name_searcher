2ffef0cc68c27f7e89e2e7d7ae9c18be3d9f9d07
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-02-08 18:54:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
-id = [
-# сюда id
-]
+file = 'id.txt' # файл с id страницами
+ofile = open(file)
 
 f = open('data.txt','w')
-for i in id:
-    url=f'https://vk.com/id{i}'
+for i in ofile:
+    id_page = i.replace(',','').rstrip()
+    url=f'https://vk.com/id{id_page}'
     html_content = requests.get(url).text
     soup = BeautifulSoup(html_content, "html5lib") 
+    print(str(soup.title).replace('<title>','').replace('</title>','').replace('|','').replace('ВКонтакте','').rstrip()+',')
     f.write((str(soup.title).replace('<title>','').replace('</title>','').replace('|','').replace('ВКонтакте','')).rstrip()+',\n')
 f.close()
